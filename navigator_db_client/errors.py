94a6df996a3a119dceb65638d7dd94206e4fc838
AdrianPayne <adriansacristan1993@gmail.com>
2024-01-24 17:26:50 +0000

    PDCT-716 add all error types

--- navigator_db_client/errors.py
+++ navigator_db_client/errors.py
@@ -10,7 +10,32 @@ class ExceptionWithMessage(Exception):
         """
         return self.args[0] if len(self.args) > 0 else "<no message>"
 
+
+class AuthenticationError(ExceptionWithMessage):
+    """Raised when authentication fails."""
+
+    pass
+
+
 class RepositoryError(ExceptionWithMessage):
     """Raised when something fails in the database."""
+
+    pass
+
+
+class TokenError(ExceptionWithMessage):
+    """Raised when there is an error encoding/decoding the token."""
+
+    pass
+
+
+class ValidationError(ExceptionWithMessage):
+    """Raised when validation fails."""
+
+    pass
+
+
+class AuthorisationError(ExceptionWithMessage):
+    """Raised when authentication fails."""
 
     pass
