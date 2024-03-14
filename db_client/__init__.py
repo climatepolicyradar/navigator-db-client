cc850f0ad0e127459b523fd328c562002ec6482b
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-03-14 12:10:28 +0000

    Exported function and module symbols using __all__

--- db_client/__init__.py
+++ db_client/__init__.py
@@ -1,5 +1,6 @@
 """ Export select function and module symbols. """
 
+import db_client.run_migrations as run_migrations_script
 from db_client.run_migrations import run_migrations
 
-__all__ = ("run_migrations",)
+__all__ = ("run_migrations", "run_migrations_script")
