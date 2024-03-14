6325e9051b84bd16b3d2713b5af8c2e0609f0437
AdrianPayne <adriansacristan1993@gmail.com>
2024-03-14 16:28:58 +0900

    clean conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,16 @@
 # import contextlib
 import logging
 import os
-import psycopg2
 
 import pytest
 from pytest_alembic.config import Config
-from sqlalchemy import MetaData, create_engine
-from sqlalchemy_utils import create_database, database_exists, drop_database
+from sqlalchemy_utils import create_database, database_exists
 
 from db_client.models import Base
 
 from pytest_mock_resources import create_postgres_fixture, PostgresConfig
 
-# from tests.test_schema.helpers import clean_tables
+from db_client.utils import get_library_path
 
 test_db_url = str(os.getenv("DATABASE_URL"))
 
@@ -23,9 +21,9 @@ _LOGGER.info("test")
 @pytest.fixture
 def alembic_config():
     """Override this fixture to configure the exact alembic context setup required."""
-    root_dir = os.path.dirname(os.path.dirname(__file__))
-    alembic_ini_path = os.path.join(root_dir, "db_client", "alembic.ini")
-    alembic_scripts_path = os.path.join(root_dir, "db_client", "alembic")
+    root_dir = get_library_path()
+    alembic_ini_path = os.path.join(root_dir, "alembic.ini")
+    alembic_scripts_path = os.path.join(root_dir, "alembic")
     return Config(
         config_options={
             "file": alembic_ini_path,
@@ -34,21 +32,20 @@ def alembic_config():
         }
     )
 
-# Configuration of postgres fixture
+# Configuration of pytest mock postgres fixtures
 @pytest.fixture(scope='session')
 def pmr_postgres_config():
     return PostgresConfig(image='postgres:14')
 
-# Postgres fixture (using Docker implicily) for alembic tests
+# Engine Postgres fixture for alembic tests
 alembic_engine = create_postgres_fixture()
+
+# Engine Postgres fixture for our custom tests
 test_engine_fixture = create_postgres_fixture()
 
 @pytest.fixture()
 def test_engine(test_engine_fixture):
-    """Create a test database and use it for the whole test session."""
-    metadata = MetaData(bind=test_engine_fixture)
-    
-    # Asegúrate de que tu URL de conexión es la correcta para el test_engine_fixture
+    """Create a test database and use it for the whole test session."""    
     if not database_exists(test_engine_fixture.url):
         create_database(test_engine_fixture.url)
     
@@ -58,6 +55,3 @@ def test_engine(test_engine_fixture):
 
     # Run the tests
     yield test_engine_fixture
-
-    # Eliminar las tablas después de la ejecución de las pruebas
-    Base.metadata.drop_all(test_engine_fixture)
