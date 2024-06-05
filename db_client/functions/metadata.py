06e54808d89479c77100717d14fe30c44ef6f586
diversemix <diversemix@gmail.com>
2024-06-05 12:43:09 +0100

    add more tests

--- db_client/functions/metadata.py
+++ db_client/functions/metadata.py
@@ -1,14 +1,42 @@
-from typing import Sequence
+from typing import Any, Sequence
 
 from sqlalchemy.orm import Session
 
 from db_client.models.dfce.family import Family, FamilyCorpus
 from db_client.models.dfce.metadata import FamilyMetadata
+from db_client.models.dfce.taxonomy_entry import TaxonomyEntry
 from db_client.models.organisation.corpus import Corpus, CorpusType
 
 MetadataValidationResult = Sequence[str]
 
 # TODO use the MetadataValidationResult as the return value
+# class MetadataValidationError(Exception):
+#     """An Error that occurs when validating metadata"""
+
+#     @property
+#     def message(self) -> str:
+#         """
+#         Returns the message for the exception.
+
+#         :return str: The message string.
+#         """
+#         return self.args[0] if len(self.args) > 0 else "<no message>"
+
+
+def validate_taxonomy(taxonomy: dict[str, Any]) -> MetadataValidationResult:
+    """Validates the Taxonomy.
+
+    :param dict[str, Any] taxonomy: The taxonomy to validate.
+    """
+    result = []
+    for entry in taxonomy:
+        if "node" in entry:
+            node = entry["node"]
+            if "name" not in node:
+                result.append("Taxonomy entry is missing 'name' field")
+        else:
+            result.append("Taxonomy entry is missing 'node' field")
+    return result
 
 
 def validate_family_metadata(db: Session, family: Family):
@@ -19,6 +47,7 @@ def validate_family_metadata(db: Session, family: Family):
     :return MetadataValidationResult: A list of errors or an empty list meaning ok
     """
 
+    # Retrieve the Taxonomy and the Family's metadata
     corpus_type, metadata = (
         db.query(CorpusType, FamilyMetadata)
         .join(Corpus, CorpusType.name == Corpus.corpus_type_name)
@@ -30,5 +59,14 @@ def validate_family_metadata(db: Session, family: Family):
     )
 
     taxonomy = corpus_type.valid_metadata
-    metadata = metadata.value
-    return [taxonomy, metadata]
+    if type(taxonomy) is not list:
+        raise TypeError("Taxonomy is not a list")
+    # Will throw a TypeError if the taxonomy is not a dict.
+    taxonomy_entries = [TaxonomyEntry(**list(entry.values())[0]) for entry in taxonomy]
+    print(taxonomy_entries)
+
+    # TODO: validate_taxonomy()
+    family_metadata = metadata.value
+
+    # TODO: validate family_metadata against taxonomy
+    return [taxonomy, family_metadata]
--- tests/functions/test_metadata.py
+++ tests/functions/test_metadata.py
@@ -1,3 +1,5 @@
+import pytest
+from pydantic import ValidationError
 from pytest_mock_resources import create_postgres_fixture
 
 from db_client.functions.metadata import validate_family_metadata
@@ -6,11 +8,115 @@ from tests.functions.helpers import family_build, metadata_build
 
 db = create_postgres_fixture(Base, session=True)
 
+"""
+Test Plan
 
-def test_this(db):
-    org, corpus, corpus_type = metadata_build(db, {})
-    family = family_build(db, org, corpus, {"this": "that"})
+- Taxonomy not a list
+- Bad Taxonomy
+- Incomplete Taxonomy
+- Extra fields in Taxonomy
 
-    assert db is not None
-    result = validate_family_metadata(db, family)
-    assert len(result) == 2
+- Blank Taxonomy allows any metadata
+
+- Good Taxonomy - bad metadata
+- Good Taxonomy - incomplete metadata
+- Good Taxonomy - extra metadata
+- Good Taxonomy - 
+
+    allow_blanks: bool
+    allowed_values: Sequence[str]
+    allow_any: bool = False
+
+"""
+
+
+def setup_test(db, taxonomy, metadata):
+    org, corpus, corpus_type = metadata_build(db, taxonomy)
+    family = family_build(db, org, corpus, metadata)
+    return org, family
+
+
+def test_validation_fails_when_taxonomy_is_not_a_list(db):
+    _, family = setup_test(db, {"taxonomy": "not a list"}, {"metadata": "anything"})
+    with pytest.raises(TypeError) as e:
+        validate_family_metadata(db, family)
+    assert e.value.args[0] == "Taxonomy is not a list"
+
+
+def test_validation_fails_when_taxonomy_bad(db):
+    _, family = setup_test(
+        db, [{"one": "two"}, {"three": "four"}], {"metadata": "anything"}
+    )
+
+    with pytest.raises(ValidationError) as e:
+        validate_family_metadata(db, family)
+
+    assert len(e.value.errors()) == 3
+
+
+def test_validation_fails_when_taxonomy_missing_allow_blanks(db):
+    _, family = setup_test(
+        db,
+        [
+            {
+                "author_type": {
+                    "allowed_values": ["Party", "Non-Party"],
+                }
+            }
+        ],
+        {"metadata": "anything"},
+    )
+    with pytest.raises(ValidationError) as e:
+        validate_family_metadata(db, family)
+
+    assert len(e.value.errors()) == 1
+    error = e.value.errors()[0]
+    assert error["msg"] == "Field required"
+    assert error["type"] == "missing"
+    assert error["loc"] == ("allow_blanks",)
+
+
+def test_validation_fails_when_taxonomy_missing_allowed_values(db):
+    _, family = setup_test(
+        db,
+        [
+            {
+                "author_type": {
+                    "allow_blanks": False,
+                }
+            }
+        ],
+        {"metadata": "anything"},
+    )
+    with pytest.raises(ValidationError) as e:
+        validate_family_metadata(db, family)
+
+    assert len(e.value.errors()) == 1
+    error = e.value.errors()[0]
+    assert error["msg"] == "Field required"
+    assert error["type"] == "missing"
+    assert error["loc"] == ("allowed_values",)
+
+
+def test_validation_fails_when_taxonomy_has_extra(db):
+    _, family = setup_test(
+        db,
+        [
+            {
+                "author_type": {
+                    "allow_blanks": False,
+                    "allowed_values": ["Party", "Non-Party"],
+                    "extra": "field",
+                }
+            }
+        ],
+        {"metadata": "anything"},
+    )
+    with pytest.raises(ValidationError) as e:
+        validate_family_metadata(db, family)
+
+    assert len(e.value.errors()) == 1
+    error = e.value.errors()[0]
+    assert error["msg"] == "Unexpected keyword argument"
+    assert error["type"] == "unexpected_keyword_argument"
+    assert error["loc"] == ("extra",)
