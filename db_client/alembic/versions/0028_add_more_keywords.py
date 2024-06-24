d1b7e1d5a5231bf67f161ce7cfc4f78fabf91161
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-06-24 16:32:55 +0100

    Move populate functions into 0028 migration

--- db_client/alembic/versions/0028_add_more_keywords.py
+++ db_client/alembic/versions/0028_add_more_keywords.py
@@ -9,26 +9,37 @@ Create Date: 2024-02-26 12:00:00
 
 import json
 from string import Template
-from typing import Callable
+from typing import Callable, Union, cast
 
 import sqlalchemy as sa
 from alembic import op
+from slugify import slugify
+from sqlalchemy import update
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 
-from db_client.data_migrations import (
-    populate_counters,
-    populate_document_role,
-    populate_document_type,
-    populate_document_variant,
-    populate_event_type,
-    populate_geo_statistics,
-    populate_geography,
-    populate_language,
-)
 from db_client.data_migrations.taxonomy_utils import read_taxonomy_values
-from db_client.data_migrations.utils import has_rows
+from db_client.data_migrations.utils import (
+    has_rows,
+    load_list,
+    load_list_idempotent,
+    load_tree,
+)
 from db_client.models import ORGANISATION_CCLW, ORGANISATION_UNFCCC
+from db_client.models.dfce.family import (
+    FamilyDocumentRole,
+    FamilyDocumentType,
+    FamilyEventType,
+    Variant,
+)
+from db_client.models.dfce.geography import (
+    CPR_DEFINED_GEOS,
+    GEO_OTHER,
+    Geography,
+    GeoStatistics,
+)
+from db_client.models.document.physical_document import Language
+from db_client.models.organisation.counters import EntityCounter
 from db_client.utils import get_library_path
 
 Base = automap_base()
@@ -134,6 +145,245 @@ def get_cclw_taxonomy():
             ]
 
     return taxonomy
+
+
+def _add_geo_slugs(geo_tree: list[dict[str, dict]]):
+    for entry in geo_tree:
+        data = entry["node"]
+        data["slug"] = slugify(data["display_value"], separator="-")
+
+        child_nodes = cast(list[dict[str, dict]], entry["children"])
+        if child_nodes:
+            _add_geo_slugs(child_nodes)
+
+
+def remove_old_international_geo(db: Session) -> None:
+    db_international = (
+        db.query(Geography).filter(Geography.value == "INT").one_or_none()
+    )
+    if db_international is not None:
+        db_stats = (
+            db.query(GeoStatistics)
+            .filter(GeoStatistics.geography_id == db_international.id)
+            .one_or_none()
+        )
+        if db_stats is not None:
+            db.delete(db_stats)
+            db.flush()
+        db.delete(db_international)
+        db.flush()
+
+
+def to_float(value: str) -> Union[float, None]:
+    first_str = value.split(" ")[0]
+    retval = None
+    try:
+        retval = float(first_str)
+    except ValueError:
+        print(f"Unparsable for float: {first_str}")
+    return retval
+
+
+def _apply_geo_statistics_updates(db: Session) -> None:
+    with open(
+        f"{get_library_path()}/data_migrations/data/geo_stats_updates.json"
+    ) as geo_stats_file:
+        geo_stats_data = json.load(geo_stats_file)
+        for geo_stat in geo_stats_data:
+            geography_id = (
+                db.query(Geography.id)
+                .filter_by(value=geo_stat["iso"], display_value=geo_stat["name"])
+                .scalar()
+            )
+            geo_stats_id = (
+                db.query(GeoStatistics.id).filter_by(geography_id=geography_id).scalar()
+            )
+            args = {**geo_stat}
+            args["geography_id"] = geography_id
+            del args["iso"]
+            result = db.execute(
+                update(GeoStatistics)
+                .values(**args)
+                .where(GeoStatistics.geography_id == geography_id)
+            )
+
+            if result.rowcount == 0:  # type: ignore
+                raise ValueError(
+                    f"In geo_stats id: {geo_stats_id} for geo: {geo_stat['name']}"
+                )
+
+
+def _populate_initial_geo_statistics(db: Session) -> None:
+    """Populates the geo_statistics table with pre-defined data."""
+
+    if has_rows(db, GeoStatistics):
+        return
+
+    # Load geo_stats data from structured data file
+    with open(
+        f"{get_library_path()}/data_migrations/data/geo_stats_data.json"
+    ) as geo_stats_file:
+        geo_stats_data = json.load(geo_stats_file)
+        for geo_stat in geo_stats_data:
+            geography_id = (
+                db.query(Geography.id)
+                .filter_by(value=geo_stat["iso"], display_value=geo_stat["name"])
+                .scalar()
+            )
+            args = {**geo_stat}
+            args["geography_id"] = geography_id
+            del args["iso"]
+            db.add(GeoStatistics(**args))
+
+
+def populate_counters(db: Session):
+    n_rows = db.query(EntityCounter).count()
+    if n_rows == 0:
+        db.add(
+            EntityCounter(
+                prefix=ORGANISATION_CCLW, description="Counter for CCLW entities"
+            )
+        )
+        db.add(
+            EntityCounter(
+                prefix=ORGANISATION_UNFCCC, description="Counter for UNFCCC entities"
+            )
+        )
+        db.commit()
+
+
+def populate_language(db: Session) -> None:
+    """Populates the langauge table with pre-defined data."""
+
+    if has_rows(db, Language):
+        return
+
+    with open(
+        f"{get_library_path()}/data_migrations/data/language_data.json"
+    ) as language_file:
+        language_data = json.load(language_file)
+        load_list(db, Language, language_data)
+
+
+def populate_document_type(db: Session) -> None:
+    """Populates the document_type table with pre-defined data."""
+
+    if has_rows(db, FamilyDocumentType):
+        return
+
+    # This is no longer fixed but additive,
+    # meaning we will add anything here that is not present in the table
+
+    with open(
+        f"{get_library_path()}/data_migrations/data/law_policy/document_type_data.json"
+    ) as submission_type_file:
+        document_type_data = json.load(submission_type_file)
+        load_list_idempotent(
+            db, FamilyDocumentType, FamilyDocumentType.name, "name", document_type_data
+        )
+
+    with open(
+        f"{get_library_path()}/data_migrations/data/unf3c/submission_type_data.json"
+    ) as submission_type_file:
+        submission_type_data = json.load(submission_type_file)
+        document_type_data = [
+            {"name": e["name"], "description": e["name"]} for e in submission_type_data
+        ]
+        load_list_idempotent(
+            db, FamilyDocumentType, FamilyDocumentType.name, "name", document_type_data
+        )
+
+
+def populate_document_role(db: Session) -> None:
+    """Populates the document_type table with pre-defined data."""
+
+    if has_rows(db, FamilyDocumentRole):
+        return
+
+    with open(
+        f"{get_library_path()}/data_migrations/data/law_policy/document_role_data.json"
+    ) as document_role_file:
+        document_role_data = json.load(document_role_file)
+        load_list(db, FamilyDocumentRole, document_role_data)
+
+
+def populate_document_variant(db: Session) -> None:
+    """Populates the document_type table with pre-defined data."""
+
+    if has_rows(db, Variant):
+        return
+
+    with open(
+        f"{get_library_path()}/data_migrations/data/law_policy/document_variant_data.json"
+    ) as document_variant_file:
+        document_variant_data = json.load(document_variant_file)
+        load_list(db, Variant, document_variant_data)
+
+
+def populate_event_type(db: Session) -> None:
+    """Populates the family_event_type table with pre-defined data."""
+
+    if has_rows(db, FamilyEventType):
+        return
+
+    with open(
+        f"{get_library_path()}/data_migrations/data/law_policy/event_type_data.json"
+    ) as event_type_file:
+        event_type_data = json.load(event_type_file)
+        load_list(db, FamilyEventType, event_type_data)
+
+
+def populate_geography(db: Session) -> None:
+    """Populates the geography table with pre-defined data."""
+
+    geo_populated = has_rows(db, Geography)
+    if geo_populated:
+        return
+
+    # First ensure our defined entries are present
+    remove_old_international_geo(db)
+
+    # Add the Other region
+    other = db.query(Geography).filter(Geography.value == GEO_OTHER).one_or_none()
+    if other is None:
+        other = Geography(
+            display_value=GEO_OTHER,
+            slug=slugify(GEO_OTHER),
+            value=GEO_OTHER,
+            type="ISO-3166 CPR Extension",
+        )
+        db.add(other)
+        db.flush()
+
+    # Add the CPR geo definitions in Other
+    for value, description in CPR_DEFINED_GEOS.items():
+        db_geo = db.query(Geography).filter(Geography.value == value).one_or_none()
+        if db_geo is None:
+            db.add(
+                Geography(
+                    display_value=description,
+                    slug=slugify(value),
+                    value=value,
+                    type="ISO-3166 CPR Extension",
+                    parent_id=other.id,
+                )
+            )
+
+    if geo_populated:
+        return
+
+    with open(
+        f"{get_library_path()}/data_migrations/data/geography_data.json"
+    ) as geo_data_file:
+        geo_data = json.loads(geo_data_file.read())
+        _add_geo_slugs(geo_data)
+        load_tree(db, Geography, geo_data)
+
+
+def populate_geo_statistics(db: Session) -> None:
+    _populate_initial_geo_statistics(db)
+    db.flush()
+    _apply_geo_statistics_updates(db)
 
 
 def populate_org_taxonomy(
