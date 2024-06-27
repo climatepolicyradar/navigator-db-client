3b776c89c722b54a97a0d44b469623336408dc89
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-06-27 13:19:44 +0100

    Remove rogue cascade

--- db_client/alembic/versions/0013_families_and_collections.py
+++ db_client/alembic/versions/0013_families_and_collections.py
@@ -234,7 +234,6 @@ def upgrade():
             ["event_type_name"],
             ["family_event_type.name"],
             name=op.f("fk_family_event__event_type_name__family_event_type"),
-            ondelete="CASCADE",
         ),
         sa.ForeignKeyConstraint(
             ["family_document_import_id"],
