9bef09f3cbfd3ab2ebf751ebb497d486b8afe384
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-07-04 17:24:19 +0100

    Fix validate_family_metadata & add validate_document_metadata

--- db_client/functions/__init__.py
+++ db_client/functions/__init__.py
@@ -4,10 +4,14 @@ from db_client.functions.dfce_helpers import (
     add_event,
     add_families,
 )
-from db_client.functions.metadata import validate_family_metadata
+from db_client.functions.metadata import (
+    validate_document_metadata,
+    validate_family_metadata,
+)
 
 __all__ = (
     "validate_family_metadata",
+    "validate_document_metadata",
     "add_collections",
     "add_families",
     "add_event",
