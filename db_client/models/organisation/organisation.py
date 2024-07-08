0ce012310d4ea036c4ced85ef859be06780f411b
diversemix <diversemix@gmail.com>
2024-07-08 13:12:40 +0100

    make organisation name unique

--- db_client/models/organisation/organisation.py
+++ db_client/models/organisation/organisation.py
@@ -9,6 +9,6 @@ class Organisation(Base):
     __tablename__ = "organisation"
 
     id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String)
+    name = sa.Column(sa.String, unique=True, nullable=False)
     description = sa.Column(sa.String)
     organisation_type = sa.Column(sa.String)
