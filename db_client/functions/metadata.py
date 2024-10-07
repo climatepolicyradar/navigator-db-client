66aa3d087a059f3edf4dc7dda37fbd4e631356c2
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-10-07 16:53:50 +0100

    More debug

--- db_client/functions/metadata.py
+++ db_client/functions/metadata.py
@@ -171,7 +171,8 @@ def build_valid_taxonomy(
                 f"metadata {metadata}, "
                 f"{all(k in list(taxonomy.keys()) for k in ['allow_any', 'allow_blanks', 'allowed_values'])}"
                 f"{all(k in ['allow_any', 'allow_blanks', 'allowed_values'] for k in list(taxonomy.keys()))}"
-                f"{metadata.keys() == [EntitySpecificTaxonomyKeys.EVENT.value] if metadata is not None else None}"
+                f"{metadata.keys() == [EntitySpecificTaxonomyKeys.EVENT.value] if metadata is not None else None},"
+                f"{list(metadata.keys()) == [EntitySpecificTaxonomyKeys.EVENT.value] if metadata is not None else None}"
             )
 
         # We rely on pydantic to validate the values here
