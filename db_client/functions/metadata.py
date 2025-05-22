cea6d1b633710aff433237766fd3205abf034c5c
annaCPR <anna@climatepolicyradar.org>
2025-05-22 14:19:04 +0100

    Feature/app 343 tidy up duplicated event type taxonomy (#90)
    * Remove old duplicated event taxonomy from validate_metadata
    
    * Update add_events to pull datetime_event_name from event
    
    * Bump patch version

--- db_client/functions/metadata.py
+++ db_client/functions/metadata.py
@@ -52,7 +52,6 @@ def validate_metadata(
             if k
             not in [
                 EntitySpecificTaxonomyKeys.DOCUMENT.value,
-                "event_type",  # TODO: Remove as part of PDCT-1622
                 EntitySpecificTaxonomyKeys.EVENT.value,
                 EntitySpecificTaxonomyKeys.COLLECTION.value,
             ]
