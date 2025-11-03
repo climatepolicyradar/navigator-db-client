5f677d94cb90a60b5b385ed5f3db4cf1e9a31693
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-11-03 14:54:29 +0000

    Attempt to fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -60,17 +60,18 @@ def template_db_engine(template_engine_fixture):
         engine = create_engine(db_url)
         connection = engine.connect()
         run_migrations(engine)
+        connection.close()
+
+        yield engine
     finally:
         if connection is not None:
             connection.close()
         if engine is not None:
             engine.dispose()
 
-    yield engine
-
-    # Cleanup
-    if database_exists(db_url):
-        drop_database(db_url)
+        # Cleanup
+        if database_exists(db_url):
+            drop_database(db_url)
 
 
 @pytest.fixture(scope="function")
